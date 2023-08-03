edceb4665f607d80003b495353087eb63eb94b96
Shivam Bansal <shivam.bansal@autodesk.com>
2023-08-03 18:57:54 +0530

    trying

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 
+const got = require('got');
+got.emitter.setMaxListeners(20);
+
 app.get('/', (req, res) => { 
   res.send('Hello World!!!'); 
 });
