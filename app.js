3b4bf2b5d6458325b5e2db941db83b47cae59964
Shivam Bansal <shivam.bansal@autodesk.com>
2023-08-03 19:15:42 +0530

    retry

--- app.js
+++ app.js
@@ -1,15 +1,25 @@
 const express = require('express');
 const app = express();
 
-const got = require('got');
-got.emitter.setMaxListeners(20);
+const http = require('http');
+const server = http.createServer(app);
+
+server.setMaxListeners(0);
 
 app.get('/', (req, res) => { 
   res.send('Hello World!!!'); 
 });
 
+const customListener = () => {
+  console.log('Custom Event');
+};
+
+server.on('customEvent',customListener);
+server.emit('customEvent');
+server.off('customEvent',customListener);
+
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
 });
 
-module.exports = app;
+module.exports = server;
